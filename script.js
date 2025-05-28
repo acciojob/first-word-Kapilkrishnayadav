fc10792ea2e7ae54ac8b17f88b92bfab2698da77
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-28 15:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+	let trimS=s.trim();
+	let ans="";
+	for(let ch of trimS)
+		{
+			if(ch==" ")
+				return ans;
+			ans=ans+ch;
+				
+		}
+	return ans;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
